ed10d56056c56439e7653fc128a07b6e6d50cdc3
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-05-30 21:16:20 +0530

    third commit

---
+++ Day3/day3_Q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int start,end,i,j,prime;
+    printf("Enter the starting range: ");
+    scanf("%d",&start);
+    printf("Enter the ending range: ");
+    scanf("%d", &end);
+    for(i =start;i<=end;i++)
+    {
+    if(i <= 1)
+    continue;
+    prime = 1;
+    for(j=2; j< i; j++)
+    {
+    if(i % j == 0)
+    { prime = 0;
+    break;
+    }
+    }
+    if(prime == 1)
+ printf("%d ", i);}
+    return 0;
+}
