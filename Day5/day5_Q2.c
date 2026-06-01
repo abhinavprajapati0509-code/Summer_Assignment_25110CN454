9e5cd9a16c5fee778a44fa651a59987032766f25
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-06-01 19:35:10 +0530

    fifth commit

---
+++ Day5/day5_Q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int n,temp,rem,fact,sum =0;
+    printf("Enter a number the check : ");
+    scanf("%d",&n);
+    temp = n;
+    while(temp > 0)
+    {
+    rem = temp % 10;
+    fact = 1;
+    for(int i = 1; i <= rem; i++)
+    {
+    fact = fact * i;
+    }
+    sum = sum + fact;
+    temp = temp / 10;
+    }
+    if(sum == n)
+    {
+    printf("Strong Number");
+    }
+    else
+    printf("Not a Strong Number");
+    return 0;
+}
