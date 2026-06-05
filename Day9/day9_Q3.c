36e4beb8171fb826bb39b96b700f558a950bbbf6
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-06-05 19:24:53 +0530

    ninth commit

---
+++ Day9/day9_Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+ {
+    int n=5;
+    for (int i =1 ; i<=n ; i++)
+ {
+ for (int j =65 ; j< 65+i; j++)
+ {
+    printf("%c",64+i);
+}
+printf("\n");
+}
+return 0;
+}
