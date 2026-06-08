7fbd39fe13f24830fa316dc450c7e6fc1f6db5bc
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-06-08 17:41:12 +0530

    teelfth commit

---
+++ Day12/day12_Q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int armstrong(int n)
+{
+    int sum = 0, temp = n, rem;
+    while(temp > 0)
+    {
+        rem = temp % 10;
+        sum = sum + rem * rem * rem;
+        temp = temp / 10;
+    }
+    if(sum == n)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(armstrong(num))
+        printf("enterre numberis Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+    return 0;
+}
