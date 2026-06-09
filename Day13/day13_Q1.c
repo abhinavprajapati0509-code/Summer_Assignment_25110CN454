80a3c006a472b437581615f1a0e3835899841b03
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-06-09 13:32:48 +0530

    thirteenth commit

---
+++ Day13/day13_Q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int arr[10], n, i;
+    printf("size of hte array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array elements are:\n");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
