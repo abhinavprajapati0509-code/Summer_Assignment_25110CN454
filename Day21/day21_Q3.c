38ee624e95340914ea4d7477fa1199bde9ceec0a
abhinavprajapati0509-code <abhinavprajapati.0905@gmail.com>
2026-06-21 13:05:45 +0530

    twenty oneth commit

---
+++ Day21/day21_Q3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, v = 0, c = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||
+           str[i]=='o' || str[i]=='u' || str[i]=='A' ||
+           str[i]=='E' || str[i]=='I' || str[i]=='O' ||
+           str[i]=='U')
+        {
+            v++;
+        }
+        else if((str[i]>='A' && str[i]<='Z') ||
+                (str[i]>='a' && str[i]<='z'))
+        {
+            c++;
+        }
+    }
+
+    printf("Vowels = %d\n", v);
+    printf("Consonants = %d", c);
+
+    return 0;
+}
